ab77e22f0d4e6726b10cd4f3cb988f1b5e4d7e3f
Joe <johnjewjoe@gmail.com>
2024-04-19 16:17:39 +0100

    Comments Added

--- link_list.py
+++ link_list.py
@@ -1,15 +1,39 @@
 import random
 
 class ListNode:
+    # Constructor to initialize the node object
     def __init__(self, value, next=None):
+        '''
+        Assign data to a node. In our project it will be the game scenario
+
+        Args: 
+        -value: The value of the node
+        -next: The next node in the linked list
+
+        Returns:
+        None
+        '''
         self.value = value
+        # Initialize next as null
         self.next = next
 
 class LinkedList:
     def __init__(self):
+        '''
+        Initialize the head of the linked list
+        '''
         self.head = None
 
     def append(self, value):
+        '''
+        Create a new node and append it at the end of the linked list
+
+        Args:
+        -value: The value of the node to be appended, here it will be the game scenario
+
+        Returns:
+        None
+        '''
         new_node = ListNode(value)
         if not self.head:
             self.head = new_node
@@ -24,6 +48,15 @@ class LinkedList:
     #     self.head = new_node
 
     def to_list(self):
+        '''
+        Convert the linked list to a list
+
+        Args:
+        None
+
+        Returns:
+        -elements: A list of the elements in the linked list
+        '''
         elements = []
         current = self.head
         while current:
@@ -34,10 +67,19 @@ class LinkedList:
     # def __repr__(self):
     #     return "->".join(str(item) for item in self.to_list())
 
-def get_game_scenarios(instances):
-    random.shuffle(instances)
+def get_game_scenarios(instances_list):
+    '''
+    Create a linked list of the game scenarios
+
+    Args:
+    -instances: A list of the game scenarios
+
+    Returns:
+    -linked_list: A linked list of the game scenarios
+    '''
+    random.shuffle(instances_list)
     linked_list = LinkedList()
-    for instance in instances:
+    for instance in instances_list:
         linked_list.append(instance)
     return linked_list
 
