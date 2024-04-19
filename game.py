0b785610f7e374ddcc7e88c16bfd4b602614d85a
reagan-pius <goingreaganp@gmail.com>
2024-04-19 18:39:32 +0100

    End Screen

--- game.py
+++ game.py
@@ -123,9 +123,9 @@ class Game:
         self.screen.blit(quit_button.image, quit_button.rect)
 
     def show_end_screen(self):
-        screen = pygame.image.load('images/theme.png').convert()
+        screen = pygame.image.load('Graphics/end_screen.png').convert()
         self.screen.blit(screen, (0, 0))
-        self.display_text(f'Your Luck Score is {self.luck_score}. What a day!', (0, 0, 0), (255, 255, 255), 120, 50, font_size=20)
+        self.display_text(f'Your Luck Score is {self.luck_score}. What a day!', (0, 0, 0,), (255, 255, 255), 120, 100, font_size=20)
         play_again = Button('PLAY AGAIN', (167, 66, 132), (221, 229, 13))
         quit_button = Button('QUIT', (167, 66, 132), (221, 229, 13))
         play_again.rect.topleft = ((600 - play_again.width)/2, 176)
