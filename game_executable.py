55f0304d741f8897d309c2779f158fae3e7f3eb8
reagan-pius <goingreaganp@gmail.com>
2024-04-22 21:54:41 +0100

    Added One Executable File

--- game_executable.py
+++ game_executable.py
@@ -1,209 +1,11 @@
-# importing important libraries
 import pygame
-import os
 import random
+import os
 from random import randint, choice
-from scenario import Scenario
-from button import Button
 from sys import exit
 
-
 """
-Below is the Game Scenarios' Linked List Module
-This module implements a linked list data structure to store and manipulate game scenarios.
-The ListNode class represents a single node in the linked list, with attributes for the node's value and the next node in the list.
-The LinkedList class represents the linked list itself, with methods for appending nodes, converting the linked list data structure to a Python list data type, and a utility functions to get the final scenarios.
-The get_game_scenarios function takes a list of game scenarios, shuffles the list randomly, and returns a linked list of the scenarios.
-
-Example:
-print(get_game_scenarios(["scenario1", "scenario2", "scenario3", "scenario4"]).to_list())
-
-Note:
-This module is designed to handle errors and exceptions, and provides informative error messages to help with debugging.
-"""
-
-
-class ListNode:
-    # Constructor to initialize the node object
-    def __init__(self, value, next=None):
-        """
-        Assign data to a node. In our project it will be the game scenario
-
-        Args:
-        -value: The value of the node
-        -next: The next node in the linked list
-
-        Returns:
-        None
-        """
-        try:
-            self.value = value
-            # Initialize next as null
-            self.next = next
-        except Exception as e:
-            print(
-                "An error occured while creating a node, please check the input values!",
-                e,
-            )
-
-
-class LinkedList:
-    def __init__(self):
-        """
-        Initialize the head of the linked list
-        """
-        try:
-            self.head = None
-            self.last = None
-        except Exception as e:
-            print("An error occured. No parameter needed!", e)
-
-    def append(self, value):
-        """
-        Create a new node and append it at the end of the linked list
-
-        Args:
-        -value: The value of the node to be appended, here it will be the game scenario
-
-        Returns:
-        None
-        """
-        try:
-            new_node = ListNode(value)
-            if not self.head:
-                self.head = new_node
-                self.last = new_node
-            else:
-                self.last.next = new_node
-                self.last = new_node
-
-            return None
-
-        except Exception as e:
-            print(
-                "An error occured while appending a node, please check the input values!",
-                e,
-            )
-
-    def to_list(self):
-        """
-        Convert the linked list to a list
-
-        Args:
-        None
-
-        Returns:
-        -elements: A list of the elements in the linked list
-        """
-        try:
-            elements = []
-            current = self.head
-            while current:
-                elements.append(current.value)
-                current = current.next
-            return elements
-        except Exception as e:
-            print(
-                "An error occured while converting the linked list to a list, please check the input values!",
-                e,
-            )
-
-
-def get_game_scenarios(instances_list):
-    """
-    Create a linked list of the game scenarios
-
-    Args:
-    -instances: A list of the game scenarios
-
-    Returns:
-    -linked_list: A linked list of the game scenarios
-    """
-    try:
-        random.shuffle(instances_list)
-        linked_list = LinkedList()
-        for instance in instances_list:
-            linked_list.append(instance)
-        return linked_list
-    except Exception as e:
-        print("List needed to be passed, please check input.", e)
-
-
-# Test
-print(
-    get_game_scenarios(["scenario1", "scenario2", "scenario3", "scenario4"]).to_list()
-)
-
-
-"""
-Next is a class that represents a scenario in the game. Scenario is basically an occurence in the "day" of this game.
-
-Each scenario has a unique number, an associated image for context, and a set of choices with possible outcomes.
-The luck difference attribute represents a random value that can affect the outcome of the scenario.
-
-The `set_cases()` method allows setting the caption, choices, and outcomes for the scenario.
-The `__str__()`  method returns a string representation of the scenario, including its number.
-
-Example:
-    scenario1 = Scenario(1, "path/to/image.jpg")
-    scenario1.set_cases(
-        "You are at a fork in the road.",
-        "Go left",
-        "You find a treasure!",
-        "You get lost.",
-        "Go right",
-        "You find a friend!",
-        "You get hurt.",
-    )
-    print(scenario1)  # Output: scenario1
-"""
-
-
-class Scenario:
-    def __init__(self, scene_num, picture_path: str):
-        self.picture_path = picture_path
-        self.cases = {}
-        self.scene_num = scene_num
-        self.luck_diff = randint(1, 20)
-
-    def set_cases(
-        self,
-        caption: str,
-        choice1: str,
-        pos_outcome1: str,
-        neg_outcome1: str,
-        choice2: str,
-        pos_outcome2: str,
-        neg_outcome2: str,
-    ) -> None:
-        """Must be two cases"""
-
-        self.caption = caption
-
-        # Case 1
-        self.cases["choice1"] = ["pos_outcome1", "pos_outcome2"]
-
-        self.cases["choice1"] = choice1
-        self.cases["pos_outcome1"] = pos_outcome1
-        self.cases["neg_outcome1"] = neg_outcome1
-
-        # Case 2
-        self.cases["choice2"] = choice2
-        self.cases["pos_outcome2"] = pos_outcome2
-        self.cases["neg_outcome2"] = neg_outcome2
-
-        return None
-
-    def __str__(self):
-        output_string = f"scenario{self.scene_num}"
-
-        return output_string
-
-
-"""
-    Next is a clickable button class for Pygame. 
-    It has a method to detect click events in the game anywhere there is a button.
-    It inherits from sprite to detect and manage button surface dimensions using rect() method
+    This is a clickable button class for Pygame.
 
     Parameters:
     text (str): The text to display on the button.
@@ -274,15 +76,200 @@ class Button(pygame.sprite.Sprite):
         return action
 
 
+"""
+This is the Game Scenarios Linked List Module
+This module implements a linked list data structure to store and manipulate game scenarios.
+The ListNode class represents a single node in the linked list, with attributes for the node's value and the next node in the list.
+The LinkedList class represents the linked list itself, with methods for appending nodes, converting the linked list data structure to a Python list data type, and a utility functions to get the final scenarios.
+The get_game_scenarios function takes a list of game scenarios, shuffles the list randomly, and returns a linked list of the scenarios.
+
+Example:
+print(get_game_scenarios(["scenario1", "scenario2", "scenario3", "scenario4"]).to_list())
+
+Note:
+This module is designed to handle errors and exceptions, and provides informative error messages to help with debugging.
+"""
+
+
+class ListNode:
+    # Constructor to initialize the node object
+    def __init__(self, value, next=None):
+        """
+        Assign data to a node. In our project it will be the game scenario
+
+        Args:
+        -value: The value of the node
+        -next: The next node in the linked list
+
+        Returns:
+        None
+        """
+        try:
+            self.value = value
+            # Initialize next as null
+            self.next = next
+        except Exception as e:
+            print(
+                "An error occured while creating a node, please check the input values!",
+                e,
+            )
+
+
+class LinkedList:
+    def __init__(self):
+        """
+        Initialize the head of the linked list
+        """
+        try:
+            self.head = None
+        except Exception as e:
+            print("An error occured. No parameter needed!", e)
+
+    def append(self, value):
+        """
+        Create a new node and append it at the end of the linked list
+
+        Args:
+        -value: The value of the node to be appended, here it will be the game scenario
+
+        Returns:
+        None
+        """
+        try:
+            new_node = ListNode(value)
+            if not self.head:
+                self.head = new_node
+                return
+            last_node = self.head
+            while last_node.next:
+                last_node = last_node.next
+            last_node.next = new_node
+        except Exception as e:
+            print(
+                "An error occured while appending a node, please check the input values!",
+                e,
+            )
+
+    def to_list(self):
+        """
+        Convert the linked list to a list
+
+        Args:
+        None
+
+        Returns:
+        -elements: A list of the elements in the linked list
+        """
+        try:
+            elements = []
+            current = self.head
+            while current:
+                elements.append(current.value)
+                current = current.next
+            return elements
+        except Exception as e:
+            print(
+                "An error occured while converting the linked list to a list, please check the input values!",
+                e,
+            )
+
+
+def get_game_scenarios(instances_list):
+    """
+    Create a linked list of the game scenarios
+
+    Args:
+    -instances: A list of the game scenarios
+
+    Returns:
+    -linked_list: A linked list of the game scenarios
+    """
+    try:
+        random.shuffle(instances_list)
+        linked_list = LinkedList()
+        for instance in instances_list:
+            linked_list.append(instance)
+        return linked_list
+    except Exception as e:
+        print("List needed to be passed, please check input.", e)
+
+
+# Test
+print(
+    get_game_scenarios(["scenario1", "scenario2", "scenario3", "scenario4"]).to_list()
+)
+
+
+"""
+This class represents a scenario in the game. Scenario is basically an occurence in the "day" of this game.
+
+Each scenario has a unique number, an associated image for context, and a set of choices with possible outcomes.
+The luck difference attribute represents a random value that can affect the outcome of the scenario.
+
+The `set_cases()` method allows setting the caption, choices, and outcomes for the scenario.
+The `__str__()`  method returns a string representation of the scenario, including its number.
+
+Example:
+    scenario1 = Scenario(1, "path/to/image.jpg")
+    scenario1.set_cases(
+        "You are at a fork in the road.",
+        "Go left",
+        "You find a treasure!",
+        "You get lost.",
+        "Go right",
+        "You find a friend!",
+        "You get hurt.",
+    )
+    print(scenario1)  # Output: scenario1
+"""
+
+
+class Scenario:
+    def __init__(self, scene_num, picture_path: str):
+        self.picture_path = picture_path
+        self.cases = {}
+        self.scene_num = scene_num
+        self.luck_diff = randint(1, 20)
+
+    def set_cases(
+        self,
+        caption: str,
+        choice1: str,
+        pos_outcome1: str,
+        neg_outcome1: str,
+        choice2: str,
+        pos_outcome2: str,
+        neg_outcome2: str,
+    ) -> None:
+        """Must be two cases"""
+
+        self.caption = caption
+
+        # Case 1
+        self.cases["choice1"] = ["pos_outcome1", "pos_outcome2"]
+
+        self.cases["choice1"] = choice1
+        self.cases["pos_outcome1"] = pos_outcome1
+        self.cases["neg_outcome1"] = neg_outcome1
+
+        # Case 2
+        self.cases["choice2"] = choice2
+        self.cases["pos_outcome2"] = pos_outcome2
+        self.cases["neg_outcome2"] = neg_outcome2
+
+        return None
+
+    def __str__(self):
+        output_string = f"scenario{self.scene_num}"
+
+        return output_string
+
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 FONT_SIZE = 12
 screen_width = 600
 screen_height = 400
-
-"""
-The tree encapsulates all existing scenarios in the game which will be randomised into a list of four scenarios using .get_path()
-"""
 
 
 class TreeNode:
@@ -561,6 +548,92 @@ def get_path(root):
             return path + get_path(root.right)
 
 
+class ListNode:
+    # Constructor to initialize the node object
+    def __init__(self, value, next=None):
+        """
+        Assign data to a node. In our project it will be the game scenario
+
+        Args:
+        -value: The value of the node
+        -next: The next node in the linked list
+
+        Returns:
+        None
+        """
+        try:
+            self.value = value
+            # Initialize next as null
+            self.next = next
+        except Exception as e:
+            print(
+                "An error occured while creating a node, please check the input values!",
+                e,
+            )
+
+
+class LinkedList:
+    def __init__(self):
+        """
+        Initialize the head of the linked list
+        """
+        try:
+            self.head = None
+            self.last = None
+        except Exception as e:
+            print("An error occured. No parameter needed!", e)
+
+    def append(self, value):
+        """
+        Create a new node and append it at the end of the linked list
+
+        Args:
+        -value: The value of the node to be appended, here it will be the game scenario
+
+        Returns:
+        None
+        """
+        try:
+            new_node = ListNode(value)
+            if not self.head:
+                self.head = new_node
+                self.last = new_node
+            else:
+                self.last.next = new_node
+                self.last = new_node
+
+            return None
+
+        except Exception as e:
+            print(
+                "An error occured while appending a node, please check the input values!",
+                e,
+            )
+
+    def to_list(self):
+        """
+        Convert the linked list to a list
+
+        Args:
+        None
+
+        Returns:
+        -elements: A list of the elements in the linked list
+        """
+        try:
+            elements = []
+            current = self.head
+            while current:
+                elements.append(current.value)
+                current = current.next
+            return elements
+        except Exception as e:
+            print(
+                "An error occured while converting the linked list to a list, please check the input values!",
+                e,
+            )
+
+
 def get_game_scenarios(instances_list):
     """
     Create a linked list of the game scenarios
@@ -578,11 +651,6 @@ def get_game_scenarios(instances_list):
         return linked_list
     except Exception as e:
         print("List needed to be passed, please check input.", e)
-
-
-"""
-This is the main game logic
-"""
 
 
 class Game:
